3916d5f76f41c87255c825e9e733cdfb36c017da
Agostina Zorzoli Galeano <agozorzoli@gmail.com>
2024-08-17 15:44:20 -0300

    Solución para los distintos 'click'

--- eventos.js
+++ eventos.js
@@ -1,13 +1,21 @@
 document.addEventListener("DOMContentLoaded", function() {
 
     const divBoton = document.querySelector(".container");
+    const boton = document.getElementById("saludar");
     
     
     function soyUnDiv() {
     alert("Hola! Soy el div");     
     }
     
+    function saludo(event) {
+    event.stopPropagation();
+    alert("¡Hola!");
+    }
+
+
     divBoton.addEventListener("click", soyUnDiv);
+    boton.addEventListener("click", saludo);
 
 });
 
